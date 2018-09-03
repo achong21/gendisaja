db4664e0ccb5d7fea65f2214386effbcd31ffc12
andy <42934114+achong21@users.noreply.github.com>
2018-09-04 00:11:03 +0700

    Add files via upload

---
+++ config/option.php
@@ -0,0 +1,3 @@
+<?
+require_once 'koneksi.php';$nama_toko='..:  Ella Boutique-Ekspresikan Gaya Anda ::.';$meta_deskripsi='Ella Boutique adalah toko fashion online, yang menyediakan segala kebutuhan fashion anda.';$meta_keyword='fashion, aneka baju, toko online.';$email_pengelola='andyprabowo22@yahoo.com';
+$nomor_hp='085786384737';$nomor_rekening='12131242425253';
